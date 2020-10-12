4d1c41c076e2dc713b3a06d956dee5a6d2df040c
Ian Robinson <ian@ianrobinson.net>
2020-10-12 14:56:54 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Find the live project here:
+
+# [The Earth Object](https://iarobinson.github.io/theEarthObject/)
---
+++ earth.js
@@ -0,0 +1,28 @@
+const theEarthObjectJS = {
+  continents: {
+    africa: {},
+    antarctica: {},
+    asia: {},
+    europe: {},
+    northAmerica: {},
+    australia: {},
+    southAmerica: {}
+  },
+  oceans: {
+    arctic: {
+      description: ""
+    },
+    atlatic: {
+      description: ""
+    },
+    pacific: {
+      description: ""
+    },
+    indian: {
+      description: ""
+    },
+    southern: {
+      description: ""
+    }
+  }
+}
