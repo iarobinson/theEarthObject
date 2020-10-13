f456f07f70afc8429694d7067742b8ca2a9a15a9
Fiona Rambarran <49721850+FionaFR@users.noreply.github.com>
2020-10-12 23:18:12 -0400

    Wikipedia Links Added
    Wikipedia Links Added

---
+++ ocean.js
@@ -0,0 +1,28 @@
+const theEarthObjectJS = {
+  continents: {
+    africa: {},
+    antarctica: {},
+    asia: {},
+    europe: {},
+    northAmerica: {},
+    australia: {},
+    southAmerica: {}
+  },
+  oceans: {
+    arctic: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Arctic_Ocean"
+    },
+    atlatic: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Atlatic_Ocean"
+    },
+    pacific: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Pacific_Ocean"
+    },
+    indian: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Indian_Ocean"
+    },
+    southern: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Southern_Ocean"
+    }
+  }
+}
