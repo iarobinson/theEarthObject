2e18d0c3ef9a6b8d033d52e57dcb5d5b700131b0
Fiona Rambarran <49721850+FionaFR@users.noreply.github.com>
2020-10-12 22:27:50 -0400

    Finished
    Wikipedia Links Added to Each Continent

--- earth.js
+++ earth.js
@@ -1,12 +1,26 @@
 const theEarthObjectJS = {
   continents: {
-    africa: {},
-    antarctica: {},
-    asia: {},
-    europe: {},
-    northAmerica: {},
-    australia: {},
-    southAmerica: {}
+    africa: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Africa"
+    },
+    antarctica: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Antarctica"
+    },
+    asia: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Asia"
+    },
+    europe: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Europe"
+    },
+    northAmerica: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/North_America"
+    },
+    australia: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/Australia"
+    },
+    southAmerica: {
+      "wikipedia_link": "https://en.wikipedia.org/wiki/South_America"
+    },
   },
   oceans: {
     arctic: {
