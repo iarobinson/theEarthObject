076cb827bbb325de02c670c27af00c3cbba9972c
REEMSHANAB <72611638+REEMSHANAB@users.noreply.github.com>
2020-10-14 09:46:10 -0400

    Update earth.js

--- earth.js
+++ earth.js
@@ -11,7 +11,157 @@ const theEarthObjectJS = {
     },
     europe: {
       "wikipedia_link": "https://en.wikipedia.org/wiki/Europe"
-    },
+      "countries": {
+       "andorra": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Andorra.png"
+      }
+      "armenia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Albania.png"
+    }
+     "austria": {
+     "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Austria.png"      
+}
+  "belarus": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Belarus.png"
+    }
+"belgium": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Belgium.png"
+    }
+"bosnia-herzegovina": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Bosnia-Herzegovina.png"
+    }
+"bulgaria": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Bulgaria.png"
+    }
+"croatia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Croatia.png"
+    }
+"cyprus": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Cyprus.png"
+    }
+"czech-republic": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Czech-Republic.png" 
+    }
+"denmark": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Denmark.png"
+    }
+"estonia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Estonia.png"
+    }
+"finland": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Finland.png"
+    }
+"france": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-France.png"
+    }
+"georgia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Georgia.png"
+    }
+"germany": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Germany.png"
+    }
+"greece": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Greece.png"
+    }
+"hungary": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Hungary.png"
+    }
+"iceland": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Iceland.png"
+    }
+"ireland": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Ireland.png"
+    }
+"italy": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Italy.png"
+    }
+"kazakhstan": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Kazakhstan.png"
+    }
+"kosovo": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Kosovo.png"
+    }
+"latvia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Latvia.png"
+    }
+"liechtenstein": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Liechtenstein.png"
+    }
+"lithuania": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Lithuania.png"
+    }
+"luxembourg": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Luxembourg.png"
+    }
+"malta": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Malta.png"
+    }
+"moldova": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Moldova.png"
+    }
+"monaco": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Monaco.png"
+    }
+"montenegro": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Montenegro"
+    }
+"netherlands": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Netherlands.png"
+    }
+"north-macedonia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-North-Macedonia.png"
+    }
+"norway": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Norway.png"
+    }
+"poland": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Poland.png"
+    }
+"portugal": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Portugal.png"
+    }
+"romania": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Romania.png"
+    }
+"russia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Russia.png"
+    }
+"san-marino": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-San-Marino.png"
+    }
+"serbia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Serbia.png"
+    }
+"slovakia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Slovakia.png"
+    }
+"slovenia": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Slovenia.png"
+    }
+"spain": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Spain.png"
+    }
+"sweden": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Sweden.png"
+    }
+"switzerland": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Switzerland.png"
+    }
+"turkey": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Turkey.png"
+    }
+"ukraine": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Ukraine.png"
+    }
+"united-kingdom": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-United-Kingdom.png"
+    }
+"vatican-city": {
+      "flag": "https://www.countries-ofthe-world.com/flags-normal/flag-of-Vatican-City.png"
+    }
+}
+},
+    
     northAmerica: {
       "wikipedia_link": "https://en.wikipedia.org/wiki/North_America"
     },
