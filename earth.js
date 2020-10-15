8069763a7ea0af43cb91f468052c36ed197b13e5
ravidyarev <ravidya_rev@yahoo.com>
2020-10-14 21:45:34 -0400

    Update earth.js

--- earth.js
+++ earth.js
@@ -43,5 +43,88 @@ const theEarthObjectJS = {
       description: "",
       wikipedia_link: "https://wikipedia.org/wiki/Southern_Ocean"
     }
-  }
+  },
+   countries: {
+    oceania: {
+      nationStates: {
+	      "Australia": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Australia"
+			  },
+	      "Papua New Guinea": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Papua_New_Guinea"
+			  },
+	      "New Zealand": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/New_Zealand"
+			  },
+	      "Fiji": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Fiji"
+			  },
+	      "Solomon Islands": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Solomon_Islands"
+			  },
+	      "Vanuatu": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Vanuatu"
+			  },
+	      "Samoa": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Samoa"
+			  },
+	      "Kiribati": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Kiribati"
+			  },
+	      "Federated States of Micronesia": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Federated_States_of_Micronesia"
+			  },
+	      "Tonga": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Tonga"
+			  },
+	      "Marshall Islands": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Marshall_Islands"
+			  },
+	      "Palau": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Palau"
+			  },
+	      "Tuvalu": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Tuvalu"
+			  },
+	      "Nauru": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Nauru"
+			  }
+      },
+      dependentTerritories: {
+	      "New Caledonia (France)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/New_Caledonia"
+			  },
+	      "French Polynesia (France)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/French_Polynesia"
+			  },
+	      "Guam (US)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Guam"
+			  },
+	      "American Samoa (US)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/American_Samoa"
+			  },
+	      "Northern Mariana Islands (US)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Northern_Mariana_Islands"
+			  },
+	      "Cook Islands (NZ)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Cook_Islands"
+			  },
+	      "Wallis and Futuna (France)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Wallis_and_Futuna"
+			  },
+	      "Norfolk Island (Australia)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Norfolk_Island"
+			  },
+	      "Niue (NZ)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Niue"
+			  },
+	      "Tokelau (NZ)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Tokelau"
+			  },
+	      "Pitcairn Islands (UK)": {
+			  wikipedia_link: "https://en.wikipedia.org/wiki/Pitcairn_Islands"
+			  }
+	   }
+      }
+   }
 }
